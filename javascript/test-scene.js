a90d4c18bcf97cf64f50cf153804e23a9386fbec
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-08-20 16:00:14 +1000

    Create orbital controls and basic solar system

---
+++ javascript/test-scene.js
@@ -0,0 +1,53 @@
+import * as THREE from '/node_modules/three/build/three.module.js';
+import { scene } from '../main.js';
+
+function CreatePlanet(radius, hexColor) {
+    var planet = new THREE.Mesh(
+         new THREE.SphereGeometry( radius, 32, 16 ), 
+         new THREE.MeshBasicMaterial( { color: hexColor  } ) ); 
+         
+    scene.add( planet );
+    return planet
+}
+
+export function CreateTestSolarSystemScene() {
+    var Sun = CreatePlanet(5, 0xFFFFFF);
+    Sun.position.set(0, 0, 0);
+
+    var mecury = CreatePlanet(0.3, 0xC7C7C7);
+    mecury.position.set(8, 0, 0);
+
+    var venus = CreatePlanet(0.5, 0xFFC7C7);
+    venus.position.set(12, 0, 0);
+
+    var earth = CreatePlanet(0.5, 0x0000FF);
+    earth.position.set(16, 0, 0);
+
+    var mars = CreatePlanet(0.4, 0xFF0000);
+    mars.position.set(20, 0, 0);
+
+    var jupiter = CreatePlanet(1.5, 0xFFC7C7);
+    jupiter.position.set(32, 0, 0);
+
+    var saturn = CreatePlanet(1.2, 0xFFC7C7);
+    saturn.position.set(40, 0, 0);
+
+    var uranus = CreatePlanet(0.8, 0xFFC7C7);
+    uranus.position.set(56, 0, 0);
+
+    var neptune = CreatePlanet(0.8, 0xFFC7C7);
+    neptune.position.set(70, 0, 0);
+
+    var pluto = CreatePlanet(0.2, 0xFFC7C7);
+    pluto.position.set(90, 0, 0);
+
+    // Sun.position.set(0, 0, 0);
+
+    // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    // var cube = new THREE.Mesh( geometry, material );
+    // console.log(cube.position);
+    // scene.add( cube );
+
+    console.log("Generating solar system");
+}
