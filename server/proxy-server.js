8c20d5a073593327737a7881c431c0255c9cbf86
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-10-01 00:53:08 +1000

    proxy now running with seperate setup

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const https = require('https');
+const cors = require('cors');
 const app = express();
 const port = 8080;
 const host = "localhost";
@@ -9,6 +10,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    cors();
     next();
 });
 
@@ -17,35 +19,66 @@ app.get('/proxy', (req, res) => {
 
     console.log(targetUrl);
 
-    https.get(targetUrl, (responseFromTarget) => {
-        if (responseFromTarget.statusCode !== 200) {
-            console.error(`Error: ${responseFromTarget.statusCode} ${responseFromTarget.statusMessage}`);
-            res.status(responseFromTarget.statusCode).send(`Error: ${responseFromTarget.statusCode} ${responseFromTarget.statusMessage}`);
-            return;
-        }
+    const contentType = req.headers['content-type'];
+    res.setHeader('Content-Type', contentType);
 
-        //res.writeHead(targetRes.statusCode, responseFromTarget.headers);
+    const targetRequest = https.request(targetUrl, (targetResponse) => {
+        res.status(targetResponse.statusCode);
+        res.set(targetResponse.headers);
 
-        // const contentType = responseFromTarget.headers['content-type'];
-        // res.setHeader('Content-Type', "text/plain");
+        targetResponse.pipe(res, {
+            end: true,
+        })
+    });
 
-        // console.log(req.headers['content-type']);
-
-        // let data = '';
-
-        // // responseFromTarget.on('data', (chunk) => {
-        // //     data += chunk;
-        // // });
-        // responseFromTarget.pipe;
-
-        responseFromTarget.on('end', () => {
-            res.send(data);  // Send the response data back to the client
-        });
-    }).on('error', (error) => {
-        console.log(`Error: ${error.message}`);
+    targetRequest.on('error', (error) => {
+        console.error(`Error: ${error.message}`);
         res.status(500).send(`Error: ${error.message}`);
     });
- });
+
+    if (req.method === 'POST' || req.method === 'PUT') {
+        console.log('Forwarding request body...');
+        req.pipe(targetRequest, {
+            end: true,
+        });
+    } else {
+        console.log('No request body to forward');
+        targetRequest.end();
+    }
+}).on('error', (error) => {
+    console.log(`Error: ${error.message}`);
+    res.status(500).send(`Error: ${error.message}`);
+});
+
+//     https.get(targetUrl, (responseFromTarget) => {
+//         if (responseFromTarget.statusCode !== 200) {
+//             console.error(`Error: ${responseFromTarget.statusCode} ${responseFromTarget.statusMessage}`);
+//             res.status(responseFromTarget.statusCode).send(`Error: ${responseFromTarget.statusCode} ${responseFromTarget.statusMessage}`);
+//             return;
+//         }
+
+//         //res.writeHead(targetRes.statusCode, responseFromTarget.headers);
+
+//         // const contentType = responseFromTarget.headers['content-type'];
+//         // res.setHeader('Content-Type', "text/plain");
+
+//         // console.log(req.headers['content-type']);
+
+//         // let data = '';
+
+//         // // responseFromTarget.on('data', (chunk) => {
+//         // //     data += chunk;
+//         // // });
+//         // responseFromTarget.pipe;
+
+//         responseFromTarget.on('end', () => {
+//             res.send(data);  // Send the response data back to the client
+//         });
+//     }).on('error', (error) => {
+//         console.log(`Error: ${error.message}`);
+//         res.status(500).send(`Error: ${error.message}`);
+//     });
+//  });
 
 app.listen(port, host, () => {
     console.log(`This is a proxy server on port ${port} at host ${host}.`);
