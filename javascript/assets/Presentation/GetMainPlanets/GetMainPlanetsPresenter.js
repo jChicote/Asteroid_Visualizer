358db4ef696334df91ae7cfc55475bf57756d884
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-10-09 22:13:24 +1100

    Create presenter class

---
+++ javascript/assets/Presentation/GetMainPlanets/GetMainPlanetsPresenter.js
@@ -0,0 +1,14 @@
+
+class GetMainPlanetsPresenter {
+    constructor() {
+
+    }
+
+    async presentMainPlanetsAsync(planetData) {
+
+    }
+}
+
+// TODO: We need to move the services folder out
+// TODO: Refactor the extraction logic from the gateway to the presenter
+// TODO: We need a base presenter class for holding the result to be then used by the controller
