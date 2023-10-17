13b73a4571a0a4b517be72bbdb2d3aa95a27cbe7
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-10-18 08:30:20 +1100

    Implement ESLint into project

--- javascript/assets/infrastructure/DependencyInjection/BackEndServiceRegistration.js
+++ javascript/assets/infrastructure/DependencyInjection/BackEndServiceRegistration.js
@@ -7,29 +7,29 @@ import { HorizonsApiGateway } from '../gateways/horizons-gateway.js';
 /**
  * Registers all the dependencies from the backend application.
  */
-export function RegisterBackendServices(container){
-    RegisterGateways(container);
-    RegisterApplication(container);
-    RegisterControllers(container);
-    RegisterControllers(container);
-    RegisterPresentation(container);
+export function RegisterBackendServices (container) {
+  RegisterGateways(container);
+  RegisterApplication(container);
+  RegisterControllers(container);
+  RegisterControllers(container);
+  RegisterPresentation(container);
 }
 
-function RegisterApplication(container) {
-    container.RegisterService(GetMainPlanetInteractor, {HorizonsApiGateway});
+function RegisterApplication (container) {
+    container.RegisterService(GetMainPlanetInteractor, { HorizonsApiGateway });
 }
 
 /**
  * Register controllers for Dependency Injection.
  */
-function RegisterControllers(container) {
+function RegisterControllers (container) {
     container.RegisterService(PlanetsController, {ServiceProvider});
 }
 
 /**
  * Register presenters for Dependency Injection.
  */
-function RegisterPresentation(container) {
+function RegisterPresentation (container) {
     container.RegisterService(GetMainPlanetPresenter);
 }
 
