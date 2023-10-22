20be9b6653b89352af7c479eae8ba155b24aebf6
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-10-22 20:03:52 +1100

    Add calculations within the PlanetLocator service
    Note:
    - The calculations are functional and position the planet in the correct location
    - Old setup code from main.js has been removed and now using the new planet creator

--- javascript/game/Planets/PlanetCreationSystem.js
+++ javascript/game/Planets/PlanetCreationSystem.js
@@ -1,34 +1,37 @@
 import { PlanetCodes } from "../../assets/Framework/Infrastructure/Gateways/HorizonsApiGateway.js";
 import { PlanetsController } from "../../assets/Framework/Controllers/PlanetsController.js";
-// import { GetMainPlanetQuery } from "../../assets/Framework/Presentation/GetMainPlanet/GetMainPlanetQuery";
+import { PlanetLocator } from "./PlanetLocator.js";
+import { GetMainPlanetQuery } from "../../assets/Framework/Presentation/GetMainPlanet/GetMainPlanetQuery.js";
 import { SetVector } from "../../utils/math-library.js";
 import * as THREE from "../../../node_modules/three/build/three.module.js";
 
 export class PlanetCreationSystem {
     constructor(serviceProvider, scene) {
         this.planetController = serviceProvider.GetService(PlanetsController);
+        this.planetLocator = new PlanetLocator();
         this.scene = scene;
     }
 
     async CreateMainPlanets() {
-        this.CreatePlanet(PlanetCodes.Mercury);
-        // this.CreatePlanet(PlanetCodes.Venus);
-        // this.CreatePlanet(PlanetCodes.Earth);
-        // this.CreatePlanet(PlanetCodes.Mars);
+        // TODO: Information should be pre-loaded before any rendering
+        await this.CreatePlanet(PlanetCodes.Mercury);
+        await this.CreatePlanet(PlanetCodes.Venus);
+        await this.CreatePlanet(PlanetCodes.Earth);
+        await this.CreatePlanet(PlanetCodes.Mars);
 
-        // this.CreatePlanet(PlanetCodes.Jupiter);
-        // this.CreatePlanet(PlanetCodes.Saturn);
-        // this.CreatePlanet(PlanetCodes.Uranus);
-        // this.CreatePlanet(PlanetCodes.Neptune);
-        // this.CreatePlanet(PlanetCodes.Pluto);
+        await this.CreatePlanet(PlanetCodes.Jupiter);
+        await this.CreatePlanet(PlanetCodes.Saturn);
+        await this.CreatePlanet(PlanetCodes.Uranus);
+        await this.CreatePlanet(PlanetCodes.Neptune);
+        await this.CreatePlanet(PlanetCodes.Pluto);
     }
 
     async CreatePlanet(planetCode) {
-        // Gets planet data
-        // const planetViewModel = await this.planetController.GetMainPlanetAsync(new GetMainPlanetQuery(planetCode));
-        const planetPosition = { x: 15, y: 0, z: 15 }; // TODO: Calculate planet position with seperate service or methods
+        // Gets planet data.
+        const planetViewModel = await this.planetController.GetMainPlanetAsync(new GetMainPlanetQuery(planetCode));
 
-        const planetObject = this.RenderPlanet(1, 0xFFC7C7, planetPosition);
+        const planetPosition = await this.planetLocator.CalculatePlanetPosition(planetViewModel.result);
+        const planetObject = this.RenderPlanet(planetCode > 500 ? 12 : 1, 0xFFC7C7, planetPosition);
 
         return planetObject;
     }
