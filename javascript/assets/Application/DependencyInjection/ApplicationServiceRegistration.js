cdbe42a5a03734f5a0b4fbc7ebc15d417d831d47
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-11-05 23:24:52 +1100

    Implement functional mappings for CreatePlanetInteractor

--- javascript/assets/Application/DependencyInjection/ApplicationServiceRegistration.js
+++ javascript/assets/Application/DependencyInjection/ApplicationServiceRegistration.js
@@ -1,11 +1,18 @@
 import { PlanetRepository } from "../../Domain/Repositories/PlanetRepository.js";
 import { CreatePlanetInteractor } from "../UseCases/CreatePlanet/CreatePlanetInteractor.js";
+import { CreatePlanetMapperConfiguration } from "../UseCases/CreatePlanet/CreatePlanetMapperConfiguration.js";
 import { GetPlanetsInteractor } from "../UseCases/GetPlanets/GetPlanetsInteractor.js";
+import { ObjectMapper } from "../../../shared/Infrastructure/Mapper/ObjectMapper.js";
 
 /**
  * Registers all the dependencies from the application backend.
  */
 export function RegisterApplicationServices(container) {
-    container.RegisterService(CreatePlanetInteractor, { PlanetRepository });
+    container.RegisterService(CreatePlanetInteractor, { ObjectMapper, PlanetRepository });
     container.RegisterService(GetPlanetsInteractor, { PlanetRepository });
+}
+
+export function ConfigureApplicationMapperConfigurations(mapper) {
+    const val = new CreatePlanetMapperConfiguration();
+    val.RegisterConfigurations(mapper);
 }
--- javascript/shared/Configuration.js
+++ javascript/shared/Configuration.js
@@ -1,21 +1,30 @@
 import { Container } from "../../main.js";
-import { RegisterApplicationServices } from "../assets/Application/DependencyInjection/ApplicationServiceRegistration.js";
+import { RegisterApplicationServices, ConfigureApplicationMapperConfigurations } from "../assets/Application/DependencyInjection/ApplicationServiceRegistration.js";
 import { RegisterDomainServices } from "../assets/Domain/DependencyInjection/DomainServiceRegistration.js";
 import { RegisterFrameworkServices } from "../assets/Framework/DependencyInjection/FrameworkServiceRegistration.js";
 import { RegisterInterfaceAdapterServices } from "../assets/InterfaceAdapters/DependencyInjection/InterfaceAdapterRegistration.js";
 import { ServiceScopes } from "./DependencyInjectionServices/ServiceContainer.js";
 import { ServiceProvider } from "./DependencyInjectionServices/ServiceProvider.js";
+import { RegisterSharedServices } from "./DependencyInjectionServices/SharedServiceRegistration.js";
+import { ObjectMapper } from "./Infrastructure/Mapper/ObjectMapper.js";
 
 export class Configuration {
     ConfigureProject() {
         const container = Container();
         container.RegisterService(ServiceProvider, {}, ServiceScopes.Singleton);
 
+        const serviceProvider = container.Resolve(ServiceProvider);
+
         // Register all services
+        RegisterSharedServices(container);
         RegisterDomainServices(container);
         RegisterApplicationServices(container);
         RegisterInterfaceAdapterServices(container);
         RegisterFrameworkServices(container);
+
+        // Configure mapper
+        const mapper = serviceProvider.GetService(ObjectMapper);
+        ConfigureApplicationMapperConfigurations(mapper);
 
         return 0;
     }
