242c96335287545eb6098ce8ecc68527caf60355
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-11-09 20:37:54 +1100

    Apply code review changes

--- javascript/game/Components/Visual/MaterialRenderer.js
+++ javascript/game/Components/Visual/MaterialRenderer.js
@@ -1,42 +1,12 @@
 import * as THREE from "../../../../node_modules/three/build/three.module.js";
 import { DefaultPlanetColor } from "../../../shared/Enumerations/DefaultPlanetColor.js";
-import { PlanetCodes } from "../../../shared/Enumerations/PlanetCodes.js";
 
 export class MaterialRenderer {
     constructor(planetCode) {
-        this.material = new THREE.MeshBasicMaterial({ color: this.GetHexCode(planetCode) });
+        this.material = new THREE.MeshBasicMaterial({ color: DefaultPlanetColor.GetColorByIdentifier(planetCode) });
     }
 
     GetMaterial() {
         return this.material;
-    }
-
-    Render(planet) {
-        // TODO: Implement behaviour to map using equi-rectangular projection
-    }
-
-    GetHexCode(planetCode) {
-        switch (planetCode) {
-            case PlanetCodes.Mercury.code:
-                return DefaultPlanetColor.Mercury.hexCode;
-            case PlanetCodes.Venus.code:
-                return DefaultPlanetColor.Venus.hexCode;
-            case PlanetCodes.Earth.code:
-                return DefaultPlanetColor.Earth.hexCode;
-            case PlanetCodes.Mars.code:
-                return DefaultPlanetColor.Mars.hexCode;
-            case PlanetCodes.Jupiter.code:
-                return DefaultPlanetColor.Jupiter.hexCode;
-            case PlanetCodes.Saturn.code:
-                return DefaultPlanetColor.Saturn.hexCode;
-            case PlanetCodes.Uranus.code:
-                return DefaultPlanetColor.Uranus.hexCode;
-            case PlanetCodes.Neptune.code:
-                return DefaultPlanetColor.Neptune.hexCode;
-            case PlanetCodes.Pluto.code:
-                return DefaultPlanetColor.Pluto.hexCode;
-            default:
-                return 0x000000;
-        }
     }
 }
--- javascript/game/Planets/PlanetCreationSystem.js
+++ javascript/game/Planets/PlanetCreationSystem.js
@@ -17,40 +17,4 @@ export class PlanetCreationSystem {
 
         return planetObjects;
     }
-
-    // CreatePlanet(planet) {
-    //     const planetPosition = this.CalculatePlanetPosition(planet);
-    //     const planetRadius = this.CalculatePlanetRadius(planet);
-    //     const planetObject = this.RenderPlanet(planetRadius, 0xFFC7C7, planetPosition); // TODO: Refactor this into the Planet gameobject
-
-    //     return new Planet(planetObject, planet.planetCode, planet);
-    // }
-
-    // RenderPlanet(radius, hexColor, planetPosition) {
-    //     const planet = new THREE.Mesh(
-    //         new THREE.SphereGeometry(radius, 32, 16),
-    //         new THREE.MeshBasicMaterial({ color: hexColor }));
-
-    //     SetVector(planet, planetPosition);
-    //     this.scene.add(planet);
-
-    //     return planet;
-    // }
-
-    // TODO: Refactor this into the Planet gameobject
-    // CalculatePlanetPosition(planetData) {
-    //     const eccentricAnomaly = planetData.meanAnomaly + planetData.eccentricity * Math.sin(planetData.meanAnomaly);
-    //     const trueAnomaly = 2 * Math.atan(Math.sqrt((1 + planetData.eccentricity) / (1 - planetData.eccentricity)) * Math.tan(eccentricAnomaly / 2));
-    //     const distanceRadiusFromSun = planetData.semiMajorAxis * (1 - planetData.eccentricity * Math.cos(eccentricAnomaly));
-
-    //     return {
-    //         x: (distanceRadiusFromSun * Math.cos(trueAnomaly)) * 0.0000005,
-    //         y: 0,
-    //         z: (distanceRadiusFromSun * Math.sin(trueAnomaly)) * 0.0000005
-    //     };
-    // }
-
-    // CalculatePlanetRadius(planetData) {
-    //     return planetData.planetRadius * 0.00005; // TODO: ABstract this to make this dynamicically scaled
-    // }
 }
