8202f0dee8716c3d9144223d31499f59cc6643a7
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-11-09 20:04:11 +1100

    Move planet creation to gameobject

--- javascript/game/Planets/PlanetCreationSystem.js
+++ javascript/game/Planets/PlanetCreationSystem.js
@@ -1,6 +1,4 @@
-import * as THREE from "../../../node_modules/three/build/three.module.js";
 import { PlanetsController } from "../../assets/Framework/Controllers/PlanetsController.js";
-import { SetVector } from "../../utils/math-library.js";
 import { Planet } from "../Entities/Planet.js";
 
 export class PlanetCreationSystem {
@@ -14,44 +12,45 @@ export class PlanetCreationSystem {
         const planetObjects = [];
 
         for (const planet of planets) {
-            planetObjects.push(this.CreatePlanet(planet));
+            planetObjects.push(new Planet(planet.planetCode, planet));
         }
 
         return planetObjects;
     }
 
-    CreatePlanet(planet) {
-        const planetPosition = this.CalculatePlanetPosition(planet);
-        const planetRadius = this.CalculatePlanetRadius(planet);
-        const planetObject = this.RenderPlanet(planetRadius, 0xFFC7C7, planetPosition);
+    // CreatePlanet(planet) {
+    //     const planetPosition = this.CalculatePlanetPosition(planet);
+    //     const planetRadius = this.CalculatePlanetRadius(planet);
+    //     const planetObject = this.RenderPlanet(planetRadius, 0xFFC7C7, planetPosition); // TODO: Refactor this into the Planet gameobject
 
-        return new Planet(planetObject, planet.planetCode, planet);
-    }
+    //     return new Planet(planetObject, planet.planetCode, planet);
+    // }
 
-    RenderPlanet(radius, hexColor, planetPosition) {
-        const planet = new THREE.Mesh(
-            new THREE.SphereGeometry(radius, 32, 16),
-            new THREE.MeshBasicMaterial({ color: hexColor }));
+    // RenderPlanet(radius, hexColor, planetPosition) {
+    //     const planet = new THREE.Mesh(
+    //         new THREE.SphereGeometry(radius, 32, 16),
+    //         new THREE.MeshBasicMaterial({ color: hexColor }));
 
-        SetVector(planet, planetPosition);
-        this.scene.add(planet);
+    //     SetVector(planet, planetPosition);
+    //     this.scene.add(planet);
 
-        return planet;
-    }
+    //     return planet;
+    // }
 
-    CalculatePlanetPosition(planetData) {
-        const eccentricAnomaly = planetData.meanAnomaly + planetData.eccentricity * Math.sin(planetData.meanAnomaly);
-        const trueAnomaly = 2 * Math.atan(Math.sqrt((1 + planetData.eccentricity) / (1 - planetData.eccentricity)) * Math.tan(eccentricAnomaly / 2));
-        const distanceRadiusFromSun = planetData.semiMajorAxis * (1 - planetData.eccentricity * Math.cos(eccentricAnomaly));
+    // TODO: Refactor this into the Planet gameobject
+    // CalculatePlanetPosition(planetData) {
+    //     const eccentricAnomaly = planetData.meanAnomaly + planetData.eccentricity * Math.sin(planetData.meanAnomaly);
+    //     const trueAnomaly = 2 * Math.atan(Math.sqrt((1 + planetData.eccentricity) / (1 - planetData.eccentricity)) * Math.tan(eccentricAnomaly / 2));
+    //     const distanceRadiusFromSun = planetData.semiMajorAxis * (1 - planetData.eccentricity * Math.cos(eccentricAnomaly));
 
-        return {
-            x: (distanceRadiusFromSun * Math.cos(trueAnomaly)) * 0.0000005,
-            y: 0,
-            z: (distanceRadiusFromSun * Math.sin(trueAnomaly)) * 0.0000005
-        };
-    }
+    //     return {
+    //         x: (distanceRadiusFromSun * Math.cos(trueAnomaly)) * 0.0000005,
+    //         y: 0,
+    //         z: (distanceRadiusFromSun * Math.sin(trueAnomaly)) * 0.0000005
+    //     };
+    // }
 
-    CalculatePlanetRadius(planetData) {
-        return planetData.planetRadius * 0.00005; // TODO: ABstract this to make this dynamicically scaled
-    }
+    // CalculatePlanetRadius(planetData) {
+    //     return planetData.planetRadius * 0.00005; // TODO: ABstract this to make this dynamicically scaled
+    // }
 }
