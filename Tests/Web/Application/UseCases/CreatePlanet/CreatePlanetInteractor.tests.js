a83a88eea7a71755d64ea55e29a49ead3e8e9c56
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-12-17 21:01:58 +1100

    Implement correct configration and successful stub tests

---
+++ Tests/Web/Application/UseCases/CreatePlanet/CreatePlanetInteractor.tests.js
@@ -0,0 +1,27 @@
+import { describe, test } from "@jest/globals";
+
+describe("Extracting Heliocentric Data", () => {
+    test("Can get eccentricity", () => {
+        expect(true).toBe(true);
+    });
+
+    test("Can get semi-major axis", () => {
+        expect(true).toBe(true);
+    });
+
+    test("Can get mean-anomaly", () => {
+        expect(true).toBe(true);
+    });
+
+    test("Can get inclination", () => {
+        expect(true).toBe(true);
+    });
+
+    test("Can get longitude of ascending node", () => {
+        expect(true).toBe(true);
+    });
+
+    test("Can get argument of perihelion", () => {
+        expect(true).toBe(true);
+    });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 export default {
     testEnvironment: "jest-environment-node",
-    transform: {}
+    transform: {},
+    verbose: true,
+    testPathIgnorePatterns: [
+        "/node_modules/"
+    ],
+    testMatch: [
+        "**/Asteroid_Visualizer/Tests/Web/Application/UseCases/**/*.tests.js"
+    ]
 };
