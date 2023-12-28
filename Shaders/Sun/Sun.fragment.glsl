bdb26024ebc42b35e06b4803093b56fedda0d7a9
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-12-28 20:49:04 +1100

    Add smooth value for noise in fragment

--- Shaders/Sun/Sun.fragment.glsl
+++ Shaders/Sun/Sun.fragment.glsl
@@ -1,6 +1,24 @@
 uniform vec3 color;
+
+/* Shader uniforms */
+uniform float octaves;
+uniform float lacunarity;
+uniform float persistence;
+uniform float gridsize;
+uniform float tiling;
+
+
 varying vec2 vUv;
 
+/* smoothens out the noise */
+float smoothValue(float x) {
+    x = clamp(x, 0.0, 1.0);
+    return x * x * (3.0 - 2.0 * x);
+}
+
+
+
 void main() {
-    gl_FragColor.rgba = vec4(color, 1.0);
+    // Square UVs with (0,0) in the center
+
 }
