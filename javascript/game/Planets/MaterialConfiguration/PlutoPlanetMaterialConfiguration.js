f9f13d963e7a0f206a29a0a6c8c4ff0fa6bbe38f
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-12-31 19:50:55 +1100

    Add texture for pluto

--- javascript/game/Planets/MaterialConfiguration/PlutoPlanetMaterialConfiguration.js
+++ javascript/game/Planets/MaterialConfiguration/PlutoPlanetMaterialConfiguration.js
@@ -1,6 +1,6 @@
 import * as THREE from "../../../../node_modules/three/build/three.module.js";
 import { DefaultPlanetColor } from "../../../shared/Enumerations/DefaultPlanetColor.js";
-import { MaterialConfiguration } from "../../Base/MaterialConfiguration.js";
+import { MaterialConfiguration, TextureMaps } from "../../Base/MaterialConfiguration.js";
 
 class PlutoPlanetMaterialConfiguration extends MaterialConfiguration {
     constructor() {
@@ -8,6 +8,11 @@ class PlutoPlanetMaterialConfiguration extends MaterialConfiguration {
 
         this.key = "999";
         this.defaultMaterial = new THREE.MeshStandardMaterial({ color: DefaultPlanetColor.Pluto });
+        this.textureConfiguration = {
+            textureMaps: new TextureMaps({
+                albedoPath: "../../../../../images/Planets/Pluto/pluto-albedo.jpg"
+            })
+        };
     }
 }
 
