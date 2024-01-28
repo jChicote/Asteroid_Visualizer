e8c0c9e5077534042c311dc684e3000cdfca9801
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-01-28 13:15:50 +1100

    Implement active and inactive icons for MenuVisibilityButton

--- javascript/user-interface/option-menu/MenuVisibilityButton.jsx
+++ javascript/user-interface/option-menu/MenuVisibilityButton.jsx
@@ -2,15 +2,28 @@ import { Component } from "react";
 import { EventMediator } from "../mediator/EventMediator.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SolarSystemVisualizer } from "../../SolarSystemVisualizer.js";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 class MenuVisibilityButton extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: false,
+            activeIcon: faEyeSlash,
+            inactiveIcon: faEye
+        };
+    }
     /* -------------------------------------------------------------------------- */
     /*                               Event Handlers                               */
     /* -------------------------------------------------------------------------- */
 
     HandleClick() {
         this.eventObserver.Notify("ToggleMenuVisibility");
+
+        this.setState((prevState) => ({
+            isActive: !prevState.isActive
+        }));
     }
 
     /* -------------------------------------------------------------------------- */
@@ -22,9 +35,10 @@ class MenuVisibilityButton extends Component {
     }
 
     render() {
+        const icon = this.state.isActive ? this.state.activeIcon : this.state.inactiveIcon;
         return (
             <button className="rounded-square-button menu-button-skin row-button" onClick={this.HandleClick.bind(this)}>
-                <FontAwesomeIcon icon={faEye} className="option-icon"/>
+                <FontAwesomeIcon icon={icon} className="option-icon"/>
             </button>
         );
     }
