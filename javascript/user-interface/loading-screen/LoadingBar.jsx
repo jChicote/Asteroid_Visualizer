7220ded67a0cdaa8ad3c387158db0e39243753b6
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-02-07 08:37:26 +1100

    Add loading bar styles

--- javascript/user-interface/loading-screen/LoadingBar.jsx
+++ javascript/user-interface/loading-screen/LoadingBar.jsx
@@ -1,10 +1,23 @@
 import { Component } from "react";
 
 class LoadingBar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            progress: 0
+        };
+    }
+
+    UpdateBarFill() {
+        const fill = document.getElementById("loading-bar-progress");
+        fill.style.width = `${this.state.progress}%`;
+    }
+
     render() {
         return (
-            <div className="loading-bar">
-                <div className="loading-bar__bar"></div>
+            <div className="loading-bar-shape loading-bar-background">
+                <div id="loading-bar-progress" className="loading-bar-fill-shape loading-bar-fill"></div>
             </div>
         );
     }
