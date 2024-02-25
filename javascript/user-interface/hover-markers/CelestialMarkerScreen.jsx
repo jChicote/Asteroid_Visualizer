369e018d74d8fa147c56796f634e97db53db614d
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-02-25 20:51:23 +1100

    Create base react components

---
+++ javascript/user-interface/hover-markers/CelestialMarkerScreen.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class CelestialMarkerScreen extends Component {
+    render() {
+        return (
+            <div className="fill-canvas">
+            </div>
+        );
+    }
+}
+
+export { CelestialMarkerScreen };
