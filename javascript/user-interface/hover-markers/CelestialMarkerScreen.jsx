a54eec3a7d4be1184642353f28da207ef083127d
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-02-29 00:01:17 +1100

    Created Marker Factory

--- javascript/user-interface/hover-markers/CelestialMarkerScreen.jsx
+++ javascript/user-interface/hover-markers/CelestialMarkerScreen.jsx
@@ -1,9 +1,20 @@
 import { Component } from "react";
+import { CelestialObjectMarker } from "./CelestialHoverMarker.jsx";
 
 class CelestialMarkerScreen extends Component {
+    CreateMarkers() {
+        var testMap = new Map();
+        testMap.set("Sun", CelestialObjectMarker);
+
+        return (
+
+        )
+    }
+
     render() {
         return (
             <div className="fill-canvas">
+                {CreateMarkers()}
             </div>
         );
     }
