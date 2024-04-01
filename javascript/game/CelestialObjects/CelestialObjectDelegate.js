99ffd54936ac43591febd34ea5b020b772f05167
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-04-01 15:52:53 +1100

    Make celestial marker screen handle both planet and sun type hover markers

---
+++ javascript/game/CelestialObjects/CelestialObjectDelegate.js
@@ -0,0 +1,15 @@
+class CelestialObjectDelegate {
+    /* -------------------------------------------------------------------------- */
+    /*                                   Methods                                  */
+    /* -------------------------------------------------------------------------- */
+
+    SetMarker(marker) { }
+
+    GetRenderedObject() { }
+
+    GetName() { }
+
+    GetType() { }
+}
+
+export { CelestialObjectDelegate };
