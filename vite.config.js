6857cac35a204a4b79acdb7d56f2664d0409dcb6
Jaiden Chicote <jaiden_chicote@outlook.com>
2024-06-23 21:50:24 +1000

    Update package.json and vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: "./", // Serve from the project root
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                about: resolve(__dirname, "pages/about.html"),
+                sources: resolve(__dirname, "pages/sources.html"),
+                visualizer: resolve(__dirname, "pages/visualizer.html")
+            }
+        },
+        outDir: "dist", // Output directory for the build
+        assetsDir: "" // Keep assets flat in the output directory
+    }
 });
